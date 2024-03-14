1cb66a7df0e67e8404754b14651f8cdc2655819b
Sakshi kalyan jadhav <sakshikalyanjadhav@Sakshis-MacBook-Air.local>
2024-03-14 17:17:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let list = document.querySelector('.container .list');
+let items =document.querySelectorAll('.container .list .slide-container');
+let dots = document.querySelectorAll('.container .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+
+next.o
